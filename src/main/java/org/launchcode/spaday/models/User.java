0a15aae788c74ea9d63c215456f90104dfc743b7
cathy <cathy.pattie@gmail.com>
2020-07-02 19:52:13 -0500

    Initial commit

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -17,18 +17,22 @@ public class User {
     }
 
     public String getUsername() {
+
         return username;
     }
 
     public void setUsername(String username) {
+
         this.username = username;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
@@ -37,6 +41,7 @@ public class User {
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 }
